1aa38119d762d09fd72844bfa0352c5235056faa
WaksKevin <wakskevin@outlook.com>
2023-09-20 12:28:47 +0300

    + glightbox for displaying providers table

--- static/js/script.js
+++ static/js/script.js
@@ -1,51 +1,17 @@
 (function () {
   "use strict";
 
-  // document.addEventListener("DOMContentLoaded", function () {
-  //   const countySelect = document.getElementById("county");
-  //   const subCountySelect = document.getElementById("sub-county");
-  //   const townSelect = document.getElementById("town");
 
-  //   function populateCountyDropdown() {
-  //     fetch(`/api/counties/`)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         data.forEach(function (county) {
-  //           countySelect.innerHTML += `<option value="${county.name}">${county.name}</option>`;
-  //         });
-  //         countySelect.disabled = false;
-  //       });
-  //   }
-
-  //   // Call the function to populate the County dropdown initially
-  //   populateCountyDropdown();
-
-  //   countySelect.addEventListener("change", function () {
-  //     const selectedCounty = countySelect.value;
-  //     // Send a GET request to the DRF endpoint for sub-counties
-  //     fetch(`/api/subcounties/?county=${selectedCounty}`)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         data.forEach(function (subcounty) {
-  //           subCountySelect.innerHTML += `<option value="${subcounty.name}">${subcounty.name}</option>`;
-  //         });
-  //         subCountySelect.disabled = false;
-  //       });
-  //   });
-
-  //   subCountySelect.addEventListener("change", function () {
-  //     const selectedSubCounty = subCountySelect.value;
-  //     // Send a GET request to the DRF endpoint for towns
-  //     fetch(`/api/towns/?subcounty=${selectedSubCounty}`)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         data.forEach(function (town) {
-  //           townSelect.innerHTML += `<option value="${town.name}">${town.name}</option>`;
-  //         });
-  //         townSelect.disabled = false;
-  //       });
-  //   });
-  // });
+  /**
+  * Glightbox
+  */
+  // Initialize GLightbox
+  const glightbox = GLightbox({
+    slideNavigation: false,
+    selector: '.glightbox',
+    width: '85vw',
+    height: '85vh',
+  });
 
 
   /**
