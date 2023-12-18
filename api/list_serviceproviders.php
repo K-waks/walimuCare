ac50f30570fca08c522bf12cf8f2a88cd948e5d9
WaksKevin <wakskevin@outlook.com>
2023-12-18 11:38:45 +0300

    bugfix: list-serviceproviders.php

--- api/list_serviceproviders.php
+++ api/list_serviceproviders.php
@@ -15,44 +15,46 @@ try {
     // Fetch results
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    class MYPDF extends TCPDF
-    {
-        //Page header
-        public function Header()
+    if (!class_exists('MYPDF')) {
+        class MYPDF extends TCPDF
         {
-            // Get current date and time in the specified timezone
-            $currentDateTime = date_create('now', new DateTimeZone('Africa/Nairobi'));
-            $formattedDateTime = $currentDateTime->format('d/m/Y H:i');
+            //Page header
+            public function Header()
+            {
+                // Get current date and time in the specified timezone
+                $currentDateTime = date_create('now', new DateTimeZone('Africa/Nairobi'));
+                $formattedDateTime = $currentDateTime->format('d/m/Y H:i');
 
-            // Logo on the left
-            $this->Image(dirname(dirname(__FILE__)) . "/static/img/icon/Afya-kwa-walimu.png", 5, 5, 27, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+                // Logo on the left
+                $this->Image(dirname(dirname(__FILE__)) . "/static/img/icon/Afya-kwa-walimu.png", 5, 5, 27, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
 
-            // Second logo on the right
-            $this->Image(dirname(dirname(__FILE__)) . "/static/img/icon/TSC.png", 264, 5, 27, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+                // Second logo on the right
+                $this->Image(dirname(dirname(__FILE__)) . "/static/img/icon/TSC.png", 264, 5, 27, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
 
-            // Title
-            $this->Ln(10);
-            $this->setFont('times', 'B', 20);
-            $this->Cell(0, 15, "TEACHERS MEDICAL SCHEME COUNTY CONTACTS LIST", 0, false, 'C', 0, '', 0, false, 'M', 'M');
+                // Title
+                $this->Ln(10);
+                $this->setFont('times', 'B', 20);
+                $this->Cell(0, 15, "TEACHERS MEDICAL SCHEME COUNTY CONTACTS LIST", 0, false, 'C', 0, '', 0, false, 'M', 'M');
 
-            // Subheading with formatted date and time
-            $this->Ln(10); // Move down 10 units
-            $this->setFont('times', '', 12);
-            $this->Cell(0, 10, "Status as at {$formattedDateTime} EAT", 0, false, 'C', 0, '', 0, false, 'M', 'M');
-        }
+                // Subheading with formatted date and time
+                $this->Ln(10); // Move down 10 units
+                $this->setFont('times', '', 12);
+                $this->Cell(0, 10, "Status as at {$formattedDateTime} EAT", 0, false, 'C', 0, '', 0, false, 'M', 'M');
+            }
 
-        // Page footer
-        public function Footer()
-        {
-            // Position at 15 mm from bottom
-            $this->setY(-15);
+            // Page footer
+            public function Footer()
+            {
+                // Position at 15 mm from bottom
+                $this->setY(-15);
 
-            // Footer text
-            $this->setFont('times', 'I', 10);
-            $this->Cell(0, 10, "For more information, contact us on +254719049799,+254719044000 or email us on mmc.customerservice@minet.co.ke", 0, false, 'C', 0, '', 0, false, 'M', 'M');
+                // Footer text
+                $this->setFont('times', 'I', 10);
+                $this->Cell(0, 10, "For more information, contact us on +254719049799,+254719044000 or email us on mmc.customerservice@minet.co.ke", 0, false, 'C', 0, '', 0, false, 'M', 'M');
 
-            // Page number
-            $this->Cell(0, 10, 'Page ' . $this->getAliasNumPage() . '/' . $this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+                // Page number
+                $this->Cell(0, 10, 'Page ' . $this->getAliasNumPage() . '/' . $this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+            }
         }
     }
 
